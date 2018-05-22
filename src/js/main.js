83b8b9b5aa82bc999d3a406c6a8fa90f89088a68
Emiliano Carreño <emiliano0662@gmail.com>
2018-05-22 16:05:47 -0500

    inicio del proyecto

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+	$("#header").load("header.html");
+	
+	$("#footer").load("footer.html");
+
+	$("#header").on('click', ".btn-menu", function (event) {
+		event.preventDefault();
+
+		var seccion = $(this).attr('href');
+
+		$('body,html').animate({ scrollTop: $(seccion).offset().top }, 1000, 'swing');
+	});
+
+	$("[data-dinaanim]").each(function () {
+
+		var $this = $(this);
+
+		$this.addClass("dinaAnim-invisible");
+
+		if ($(window).width() > 767) {
+
+			$this.appear(function () {
+
+				var delay = ($this.data("dinadelay") ? $this.data("dinadelay") : 1);
+
+				if (delay > 1) $this.css("animation-delay", delay + "ms");
+
+				$this.addClass("dinaAnim-animated");
+				$this.addClass('dinaAnim-' + $this.data("dinaanim"));
+
+				setTimeout(function () {
+
+					$this.addClass("dinaAnim-visible");
+
+				}, delay);
+
+			}, { accX: 0, accY: -150 });
+
+		} else {
+
+			$this.animate({ opacity: 1 }, 300, 'easeInOutQuad', function () { });
+		}
+	});
+
+});
